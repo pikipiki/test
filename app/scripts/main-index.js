73936b9618d183e03fc16088b109004fc06fb6aa
Franky KING PHET <franky.kp@gmail.com>
2018-01-23 13:21:25 +0100

    add: blue glowing behind screens

--- app/scripts/main-index.js
+++ app/scripts/main-index.js
@@ -10,4 +10,26 @@ $(document).ready(function() {
   $('.icon-video').magnificPopup({
     type: 'iframe'
   })
+
+  var header = $('.bg-dark-vader');
+  var range = 200;
+
+  $(window).on('scroll', function () {
+    
+    var scrollTop = $(this).scrollTop(),
+        height = header.outerHeight(),
+        offset = height / 2,
+        calc = 1 - (scrollTop - offset + range) / range;
+
+    header.css({ 'opacity': calc });
+
+    if (calc > '1') {
+      header.css({ 'opacity': 1 });
+    } else if ( calc < '0' ) {
+      header.css({ 'opacity': 0 });
+    }
+    
+  });
+
+
 })
