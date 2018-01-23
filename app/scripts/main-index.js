988cb249af93148dbc2770cf22f430453d05bfeb
Franky KING PHET <franky.kp@gmail.com>
2018-01-23 13:44:26 +0100

    add(first section): fading effect on scroll

--- app/scripts/main-index.js
+++ app/scripts/main-index.js
@@ -11,24 +11,39 @@ $(document).ready(function() {
     type: 'iframe'
   })
 
-  var header = $('.bg-dark-vader');
-  var range = 200;
+  function disappearing(el, calc) {
+    el.css({ 'opacity': calc });
+    if (calc > '1') {
+      el.css({ 'opacity': 1 });
+    } else if ( calc < '0' ) {
+      el.css({ 'opacity': 0 });
+    }    
+  }
+
+  const elementsOnFirstSection = [
+    $('.bg-dark-vader'), 
+    $('.icon-video-container'),
+    $('.slider'),
+    $('.mouse_sc'),
+    $('.slider'),
+    $('.samsung-logo'),
+    $('.video-title')
+  ];
+
+  const range = 200;
 
   $(window).on('scroll', function () {
     
-    var scrollTop = $(this).scrollTop(),
-        height = header.outerHeight(),
+    let scrollTop = $(this).scrollTop(),
+        height = elementsOnFirstSection[0].outerHeight(),
         offset = height / 2,
         calc = 1 - (scrollTop - offset + range) / range;
-
-    header.css({ 'opacity': calc });
-
-    if (calc > '1') {
-      header.css({ 'opacity': 1 });
-    } else if ( calc < '0' ) {
-      header.css({ 'opacity': 0 });
-    }
     
+      elementsOnFirstSection.forEach( (el) => {
+          disappearing(el, calc)
+        }
+      )
+        
   });
 
 
