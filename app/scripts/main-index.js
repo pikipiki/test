8387d598707dc314e808aee5ca115d941625af49
pikipiki <franky.kp@gmail.com>
2018-01-23 21:57:06 +0100

    trying to implement animation image

--- app/scripts/main-index.js
+++ app/scripts/main-index.js
@@ -43,3 +43,71 @@ $(window).on('scroll', function () {
     )
       
 });
+
+function wideAni(opt) {
+    if (opt) {
+        if ($("#wideExp").hasClass("animated")) {
+            return;
+        }
+        $("#wideExp").addClass("animated");
+        setTimeout(function() {
+            $("#wideExp").find(".left").animate({
+                marginLeft: "-100%"
+            }, 800);
+            $("#wideExp").find(".right").animate({
+                marginLeft: "100%"
+            }, 800);
+            $("#wideExp").find(".i169").animate({
+                opacity: 0
+            }, 800);
+            $("#wideExp").find(".i329").animate({
+                opacity: 1
+            }, 800, function() {
+                $("#wideExp").removeClass("animated");
+            });
+        }, 50);
+    } else {
+        if ($("#wideExp").hasClass("animated")) {
+            return;
+        }
+        $("#wideExp").addClass("animated");
+        $("#wideExp").find(".left").animate({
+            marginLeft: 0
+        }, 400);
+        $("#wideExp").find(".right").animate({
+            marginLeft: 0
+        }, 400);
+        $("#wideExp").find(".i169").animate({
+            opacity: 1
+        }, 400);
+        $("#wideExp").find(".i329").animate({
+            opacity: 0
+        }, 400, function() {
+            $("#wideExp").removeClass("animated");
+        });
+    }
+}
+
+$(window).scroll(function() {
+    var startV = 0;
+    var endV = 0;
+    var isAnimate = false;
+
+    if ($(window).width() > $(window).height()) {
+        startV = $("#wideExp").offset().top - ($(window).height() / 3);
+    } else {
+        startV = $("#wideExp").offset().top - ($(window).height() / 5);
+    }
+    endV = $("#wideExp").offset().top + $("#wideExp").height() - ($(window).height() / 3);
+
+    if ($(window).scrollTop() > startV && $(window).scrollTop() < endV) {
+        if (!isAnimate) {
+            isAnimate = true;
+            wideAni(true);
+        }
+
+    } else if ($(window).scrollTop() < startV - $(window).height() || $(window).scrollTop() > endV + $(window).height()) {
+        isAnimate = false;
+        wideAni(false);
+    }
+}).scroll();
